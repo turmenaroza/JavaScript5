3fb8e04bef5aa17476b84f3d7be8064bf5ac267e
Julia <Julia_roza@estudante.sesisenai.org.br>
2025-03-20 08:39:41 -0300

    add funcao, prompt, e arrumei coisas qu estavem erradas

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-if (numero mod 2){
-    console.log ('esse número é par');
-}else{
-    console.log ('esse númeor e ímpar')
-}
+let numero = prompt ('Digite seu número')
+
+function verificarPar (){
+        console.log(numero)
+        if (numero % 2 === 0){
+            console.log (numero +  'é um número par')
+        }else{
+            console.log (numero + 'é um número ímpar')
+        }
+    }
+
+    verificarPar();
+
