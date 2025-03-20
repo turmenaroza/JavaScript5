7243ec4290189aa5147ebc9868a7ccaccbccf425
Julia <Julia_roza@estudante.sesisenai.org.br>
2025-03-20 09:16:49 -0300

    add ;

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ function verificarPar (){
     //verificarPar();
 
 
-let n = parseFloat(prompt ('Digite seu número'))
-let n2 = parseFloat(prompt ('Digite seu número'))
+let n = parseFloat(prompt ('Digite seu número'));
+let n2 = parseFloat(prompt ('Digite seu número'));
 function calcular(n, n2){
- console.log(n + n2 )
- console.log (n - n2 )
- console.log (n * n2 )
+ console.log(n + n2 );
+ console.log (n - n2 );
+ console.log (n * n2 );
  if (n2 === 0 ){
     console.log ('essa conta é impossível');
 }else{
@@ -26,3 +26,4 @@ function calcular(n, n2){
 }
 
  calcular (n, n2)
+
