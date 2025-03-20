1debf6e86648d060bbc727f6fb86d1811642211c
Julia <Julia_roza@estudante.sesisenai.org.br>
2025-03-20 09:05:54 -0300

    add ;

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 //let numero = prompt ('Digite seu número')
 
 function verificarPar (){
-        console.log(numero)
+        console.log(numero);
         if (numero % 2 === 0){
-            console.log (numero +  'é um número par')
+            console.log (numero + ' é um número par');
         }else{
-            console.log (numero + 'é um número ímpar')
+            console.log (numero + ' é um número ímpar');
         }
     }
 
     //verificarPar();
 
 
-    
+
 function calcular(n, n2){
  console.soma (n + n2 )
  console.subtracao (n - n2 )
