bce329861b09311db35c80c89527a243e6fc7b6f
Julia <Julia_roza@estudante.sesisenai.org.br>
2025-03-20 10:03:47 -0300

    add string

--- script.js
+++ script.js
@@ -1,33 +1,39 @@
 //let numero = prompt ('Digite seu número')
 
-function verificarPar (){
-        console.log(numero);
-        if (numero % 2 === 0){
-            console.log (numero + ' é um número par');
-        }else{
-            console.log (numero + ' é um número ímpar');
-        }
+function verificarPar() {
+    console.log(numero);
+    if (numero % 2 === 0) {
+        console.log(numero + ' é um número par');
+    } else {
+        console.log(numero + ' é um número ímpar');
     }
+}
 
-    //verificarPar();
+//verificarPar();
 
 
 //let n = parseFloat(prompt ('Digite seu número'));
 //let n2 = parseFloat(prompt ('Digite seu número'));
-function calcular(n, n2){
- console.log(n + n2 );
- console.log (n - n2 );
- console.log (n * n2 );
- if (n2 === 0 ){
-    console.log ('essa conta é impossível');
-}else{
-    console.log (n / n2 );
-} 
+function calcular(n, n2) {
+    console.log(n + n2);
+    console.log(n - n2);
+    console.log(n * n2);
+    if (n2 === 0) {
+        console.log('essa conta é impossível');
+    } else {
+        console.log(n / n2);
+    }
 }
 
- //calcular (n, n2)
-let n = 10
-do{
-    console.log ( n --);
+//calcular (n, n2)
+//let n = 10
+//do{
+//console.log(n--);
+//}
+//while (n > 0);
+
+let palavra = prompt('digite uma palavra ');
+function inverterTexto() {
+    console.log(palavra.split('').reverse().join(''));
 }
-while (n > 0);
+inverterTexto(palavra)
